0d1349d724fb011f21781c72928f2869869523bf
SunnyPig <15851412061@163.com>
2016-03-15 15:52:49 +0800

    cgs

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -349,7 +349,7 @@ gulp.task('weinre', function() {
 ======================================*/
 
 gulp.task('build', function(done) {
-  var tasks = ['html', 'fonts', 'images', 'sass', 'libcss','less','libjs','js','ajax','includes','php'];
+  var tasks = ['html', 'fonts', 'images', 'sass', 'libcss','less','libjs','js','php'];
   seq('clean', tasks, done);
 });
 
